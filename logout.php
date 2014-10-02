e8a54b9d3013a7d021fcf09913a74f5fc2d59735
Saikat <rychwdh2@illinois.edu>
2014-10-01 19:35:13 -0500

    if no active session redirect to login

--- logout.php
+++ logout.php
@@ -12,9 +12,13 @@
 <body class="container">
 <?php
     session_start();
-    session_destroy();
-    echo "<h4>session stopped at:". date('l'). date('H:i'). "hrs</h4>";
-    echo "<p>you have been logged out<br>". "<a href='index.php'>Click</a>Here to login</p>";
+    if (isset($_SESSION['username'])) {
+        session_destroy();
+        echo "<h4>session stopped at:". date('l'). date('H:i'). "hrs</h4>";
+        echo "<p>you have been logged out<br>". "<a href='index.php'>Click</a>Here to login</p>";
+    } else {
+        header("Location: index.php");
+    }
 ?>
 </body>
 </html>
