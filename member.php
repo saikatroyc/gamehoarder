6b970170696956b0a0e766ce601b3e0f49adff80
Saikat <rychwdh2@illinois.edu>
2014-10-12 17:06:43 -0500

    added carousel based home page

--- member.php
+++ member.php
@@ -1,64 +1,142 @@
-<?php
+<!-- <?php
     session_start(); 
     if (isset($_SESSION['username'])) {
     } else {
         header("Location: index.php");
-    }
-?>
+    }   
+?>-->
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <meta charset="utf-8">
-    <title>Main Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>GameHoarder</title>
+
+    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
     <link href="css/mystyle.css" rel="stylesheet">
-</head>
-<body class="container">
-<nav class="navbar navbar-default navbar-inverse navbar-fixed" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand active " href="#About" id="about">About</a>
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">GameHoarder</a>
+            </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#Graphs" id="graphs">TrackUrGames</a></li>
+          <li><a href="#Lucky" id="recommend">Recommendations</a></li>
+          <li><a href="#Lucky" id="search">Search</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">MyAccount <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="change_password.php">ChangePassword</a></li>
+              <li class="divider"></li>
+              <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+        </div><!-- /.navbar-collapse -->
+        </div>
+        </div>
+      </div>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#Graphs" id="graphs">Graphs</a></li>
-        <li><a href="#Lucky" id="lucky">Feeling Lucky!</a></li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search Games">
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Welcome to Gamehoarder!!</h1>
+              <p>Your onestop store for game collections</p>
+            </div>
+          </div>
         </div>
-        <button type="button" class="btn btn-default" id="search">Search</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">MyAccount <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="change_password.php">ChangePassword</a></li>
-            <li class="divider"></li>
-            <li><a href="logout.php">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-<div id="info">
-</div>
-<!-- javascript -->
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <div class="item">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Track your games</h1>
+              <p>Keep track of all your games collection. Get interactive visualization of your games</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Recommendations</h1>
+              <p>Get intuitive recommendations of hot trending games</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn More</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div><!-- /.carousel -->
+
+    <div class="container marketing">
+
+
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-10">
+          <h2 class="featurette-heading">Description of features<span class="text-muted">Comment</span></h2>
+          <p class="lead">Detailed description of feature.</p>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2014 GameHoarder; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script>
     $("#about").on('click',
@@ -69,9 +147,9 @@
     function() {
         graphs();
     });
-    $("#lucky").on('click',
+    $("#recommend").on('click',
     function() {
-        lucky();
+        recommend();
     });
     $("#search").on('click',
     function() {
@@ -83,13 +161,14 @@
     }
     function graphs()
     {
-        document.getElementById("info").innerHTML="not implemented yet";
+        document.getElementById("info").innerHTML="TBD://track your games";
     }
-    function lucky()
+    function recommend()
     {
-        document.getElementById("info").innerHTML="i dont even know what this does";
+        document.getElementById("info").innerHTML="TBD://game recommendations";
     }
     </script>
-</body>
 <?php echo "hello ". $_SESSION['username'];?>
+  </body>
 </html>
+
