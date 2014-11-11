2ee3223db708d59763ea3bb9d9b784855bb8a560
Saikat <rychwdh2@illinois.edu>
2014-11-11 01:29:12 -0600

    added recommendation for top trending games based on user count

--- recommendations_trend.php
+++ recommendations_trend.php
@@ -77,8 +77,8 @@
                 <li class="active">
                     <a href="#">By Your Most Popular Genre</a>
                 </li>
-                <li>
-                    <a href="recommendations_trend.php">Trending</a>
+                <li class="active">
+                    <a href="#">Trending</a>
                 </li>
             </ul>
         </div>
@@ -96,7 +96,7 @@
                         require 'pdo_db_connect.php';
                         $conn = func_connect_db("gamehoarder");
                         $username=$_SESSION['username'];
-                        $game_list=func_getRecommendations($conn, $username, 10);
+                        $game_list = func_getTopGamesByUsers($conn, $username, 20);
                         $numrows = count($game_list);
                         if ($numrows > 0) {
                         echo "<div class=\"container\">
@@ -107,7 +107,7 @@
     <img src=\"" . func_getGameImage($conn, $game_list[$i]['name']) ."\" width=\"300\" height=\"240\"/>
     <div class=\"caption\">
     <h5><strong>". $game_list[$i]['name'] . "</strong></h5> 
-    <h5><strong>score : ". $game_list[$i]['score'] . "</strong></h5> 
+    <h5><strong>users : ". $game_list[$i]['count'] . "</strong></h5> 
     <p id=\"mybutton1\" class=\"btn btn-primary\" onclick=\"insertGameUser('". $game_list[$i]['name'] ."','".$_SESSION['username']. "')\">Add</p>
     </div>
 </div>";
